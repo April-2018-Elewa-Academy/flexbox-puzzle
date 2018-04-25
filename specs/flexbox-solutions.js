831e7b6e8cfb5386ee6f1227edee0de3148df761
christian1006 <37366207+christian1006@users.noreply.github.com>
2018-04-25 11:41:31 +0200

    Update flexbox-solutions.js

--- specs/flexbox-solutions.js
+++ specs/flexbox-solutions.js
@@ -1,1 +1,47 @@
 
+9-3, 16-3
+
+9-1:
+#pond {
+	display: flex;
+	flex-direction: column;
+	flex-direction: row;
+}
+
+9-2:
+
+#pond {
+	display: flex;
+	flex-direction: column-reverse;
+	flex-direction: row-reverse;
+}
+
+9-3:
+#pond {
+	display: flex;
+	flex-direction: row-reverse;
+}
+
+16-1
+#pond {
+	align-self: flex-end;
+	align-self: center;
+}
+
+16-2:
+#pond {
+	align-self: stretch;
+	align-self: baseline;
+}
+
+16-3:
+#pond {
+	align-self: flex-end;
+	align-self: stretch;
+}
+
+#pond {
+	align-self: baseline;
+}
+
+	
