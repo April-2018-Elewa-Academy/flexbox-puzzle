03e7e7d9c4cde4bdde57f1511c0236db124aaa27
Mariana Casella dos Santos <mariana.casella@88solutions.be>
2018-04-26 09:41:18 +0200

    marianas scrambles

---
+++ Scrambles-Mariana.js
@@ -0,0 +1,49 @@
+const scramble9_1 = "
+#flex-puzzle { 
+    display: flex;
+    flex-wrap: no-wrap;
+}";
+
+const scramble9_2 = "
+#a {
+    display: flex;
+    order: 3;
+  }
+  
+  #g{
+    display: flex;
+    order: 1;
+  }
+  #f{
+    display: flex;
+    order: 1;
+  }
+  #i{
+    display: flex;
+    order: 2;
+  }";
+
+const scramble9_3 = "
+#flex-puzzle { 
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+
+}
+#a {
+    display: flex;
+    order: 1;
+  }
+  
+#f{
+    display: flex;
+    order: 1;
+  }
+#b{
+    display: flex;
+    order: -1;
+  }
+#c{
+    display: flex;
+    order: 2;
+  }";
